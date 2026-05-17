3db3d151fb7a4fa60691b19371d7d4c3e73f5eb5
Jaewan Park <pona4321@snu.ac.kr>
2026-05-18 03:44:35 +0900

    feat: connect backend to MongoDB with mongoose

---
+++ backend/src/config/database.ts
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+import { env } from "./env.js";
+
+export async function connectDatabase() {
+  await mongoose.connect(env.mongodbUri, {
+    serverSelectionTimeoutMS: 1000,
+  });
+}
